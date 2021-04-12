b7fcc6c65b6aadf9d0dfb789e3192dc5b14874a6
Harsh Baliyan <harshbaliyan126@gmail.com>
2021-04-12 18:33:08 +0530

    home path variable added

--- main/decrypt_pass.py
+++ main/decrypt_pass.py
@@ -2,9 +2,11 @@ import os
 import fs 
 from fs import open_fs
 import gnupg
+from pathlib import Path
 
 def decrypt(passphrase_gpg):
-    gpg = gnupg.GPG(gnupghome="/home/floppy04/.gnupg")
+    home = str(Path.home())
+    gpg = gnupg.GPG(gnupghome=home + "/.gnupg")
     home_fs = open_fs("..")
     with open("encrypted/pass.gpg","rb") as f:
         status = gpg.decrypt_file(f, passphrase=passphrase_gpg,output="encrypted/pass")
