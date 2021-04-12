035d0990bb6456d1914cc95d25c432bb463d3664
Harsh Baliyan <harshbaliyan126@gmail.com>
2021-04-12 18:33:15 +0530

    home path variable added

--- main/encrypt_pass.py
+++ main/encrypt_pass.py
@@ -2,9 +2,11 @@ import os
 import fs 
 from fs import open_fs
 import gnupg
+from pathlib import Path
 
 def encrypt(enter_reci):
-    gpg = gnupg.GPG(gnupghome="/home/floppy04/.gnupg")
+    home = str(Path.home())
+    gpg = gnupg.GPG(gnupghome=home + "/.gnupg")
     home_fs = open_fs("..")
     with open("encrypted/pass","rb") as f:
         status = gpg.encrypt_file(f, recipients=[enter_reci], output= "encrypted/pass.gpg")
