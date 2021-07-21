5ef3e1a7d5ddd9fb28777a50f20c1150b3778ca5
Harsh Baliyan <harshbaliyan126@gmail.com>
2021-07-21 13:41:47 +0530

    remove passphrase arg from gnupg.decrypt()

--- main/decrypt_pass.py
+++ main/decrypt_pass.py
@@ -1,12 +1,12 @@
 import os
-import fs 
+import fs
 from fs import open_fs
 import gnupg
 from pathlib import Path
 
-def decrypt(passphrase_gpg):
+def decrypt():
     home = str(Path.home())
     gpg = gnupg.GPG(gnupghome=home + "/.gnupg")
     home_fs = open_fs("..")
     with open("encrypted/pass.gpg","rb") as f:
-        status = gpg.decrypt_file(f, passphrase=passphrase_gpg,output="encrypted/pass")
+        status = gpg.decrypt_file(f,output="encrypted/pass")
